469065ba6045fabf0035238ccc8ffc2d07196b08
manu <manu.panjoria666@gmail.com>
2025-05-08 11:13:33 +0530

    bug/solved-point-based

--- src/components/core/ForecastDashboardPage/ForecastSidebarMenu/SpecialModelProdcuts.jsx
+++ src/components/core/ForecastDashboardPage/ForecastSidebarMenu/SpecialModelProdcuts.jsx
@@ -43,14 +43,14 @@ const SpecialModelProducts = () => {
   const hasUserSelectedRef = useRef(false);
   const hasNavigatedRef = useRef(false);
 
-  const redirectMap = {
-    30: `${REACT_APP_ASSETS_BASE_URL_NEW}/NIOTC/niotc.html`,
-    31: '/forecast-dashboard/special-products/statiscyclone',
-    32: `${REACT_APP_ASSETS_BASE_URL_NEW}/STRIKE/strike.html`,
-    34: `${REACT_APP_ASSETS_BASE_URL_NEW}/HomePage/index.php`,
-    35: `${REACT_APP_ASSETS_BASE_URL_NEW}/india_map_pqpf.php`,
-    36: `${REACT_APP_ASSETS_BASE_URL_NEW}/india-map.php`,
-  };
+  // const redirectMap = {
+  //   30: `${REACT_APP_ASSETS_BASE_URL_NEW}/NIOTC/niotc.html`,
+  //   31: '/forecast-dashboard/special-products/statiscyclone',
+  //   32: `${REACT_APP_ASSETS_BASE_URL_NEW}/STRIKE/strike.html`,
+  //   34: `${REACT_APP_ASSETS_BASE_URL_NEW}/HomePage/index.php`,
+  //   35: `${REACT_APP_ASSETS_BASE_URL_NEW}/india_map_pqpf.php`,
+  //   36: `${REACT_APP_ASSETS_BASE_URL_NEW}/india-map.php`,
+  // };
 
   // set hasUserSelectedRef to true when a user selects a product
   useEffect(() => {
@@ -75,83 +75,14 @@ const SpecialModelProducts = () => {
     }
   }, [forecastUrl]);
 
-  // useEffect(() => {
-  //   if (
-  //     selectedProductId === 30 ||
-  //     selectedProductId === 31 ||
-  //     selectedProductId === 32 ||
-  //     selectedProductId === 34
-  //   )
-  //     return;
-
-  //   const loadForecastHour = async () => {
-  //     setLoading(true);
-  //     try {
-  //       const { hours } = await fetchForecastHours(
-  //         null,
-  //         null,
-  //         selectedProductId
-  //       );
-
-  //       if (Array.isArray(hours) && hours.length > 0) {
-  //         dispatch(setSpecialForecastHours(hours));
-  //         dispatch(setSelectedSpecialForecastHour(hours[0]));
-  //       } else {
-  //         dispatch(setSpecialForecastHours([]));
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching forecast hours:', error);
-  //       toast.error('Unexpected error occurred while fetching forecast hours.');
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   loadForecastHour();
-  // }, [selectedProductId]);
-
-  // useEffect(() => {
-  //   if (!hasUserSelectedRef.current || hasNavigatedRef.current) return;
-
-  //   if (selectedProductId === 34) {
-  //     hasNavigatedRef.current = true;
-  //     window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/HomePage/index.php`;
-  //     return;
-  //   }
-
-  //   if (selectedProductId === 30) {
-  //     hasNavigatedRef.current = true;
-  //     window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/NIOTC/niotc.html`;
-  //     return;
-  //   }
-
-  //   if (selectedProductId === 31) {
-  //     hasNavigatedRef.current = true;
-  //     navigate('/forecast-dashboard/special-products/statiscyclone');
-  //     return;
-  //   }
-
-  //   if (selectedProductId === 32) {
-  //     hasNavigatedRef.current = true;
-  //     window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/STRIKE/strike.html`;
-  //     return;
-  //   }
-  //   if (selectedProductId === 35) {
-  //     hasNavigatedRef.current = true;
-  //     window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/india_map_pqpf.php`;
-  //     return;
-  //   }
-  //   if (selectedProductId === 36) {
-  //     hasNavigatedRef.current = true;
-  //     window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/india-map.php`;
-  //     return;
-  //   }
-  // }, [selectedProductId, navigate]);
-
-  const shouldRedirect = (id) => Object.keys(redirectMap).includes(String(id));
-
   useEffect(() => {
-    if (shouldRedirect(selectedProductId)) return;
+    if (
+      selectedProductId === 30 ||
+      selectedProductId === 31 ||
+      selectedProductId === 32 ||
+      selectedProductId === 34
+    )
+      return;
 
     const loadForecastHour = async () => {
       setLoading(true);
@@ -161,6 +92,7 @@ const SpecialModelProducts = () => {
           null,
           selectedProductId
         );
+
         if (Array.isArray(hours) && hours.length > 0) {
           dispatch(setSpecialForecastHours(hours));
           dispatch(setSelectedSpecialForecastHour(hours[0]));
@@ -181,17 +113,85 @@ const SpecialModelProducts = () => {
   useEffect(() => {
     if (!hasUserSelectedRef.current || hasNavigatedRef.current) return;
 
-    const redirectUrl = redirectMap[selectedProductId];
-    if (redirectUrl) {
+    if (selectedProductId === 34) {
       hasNavigatedRef.current = true;
+      window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/HomePage/index.php`;
+      return;
+    }
 
-      if (selectedProductId === 31) {
-        navigate(redirectUrl); // internal route
-      } else {
-        window.location.href = redirectUrl; // external link
-      }
+    if (selectedProductId === 30) {
+      hasNavigatedRef.current = true;
+      window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/NIOTC/niotc.html`;
+      return;
+    }
+
+    if (selectedProductId === 31) {
+      hasNavigatedRef.current = true;
+      navigate('/forecast-dashboard/special-products/statiscyclone');
+      return;
+    }
+
+    if (selectedProductId === 32) {
+      hasNavigatedRef.current = true;
+      window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/STRIKE/strike.html`;
+      return;
+    }
+    if (selectedProductId === 35) {
+      hasNavigatedRef.current = true;
+      window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/india_map_pqpf.php`;
+      return;
+    }
+    if (selectedProductId === 36) {
+      hasNavigatedRef.current = true;
+      window.location.href = `${REACT_APP_ASSETS_BASE_URL_NEW}/india-map.php`;
+      return;
     }
   }, [selectedProductId, navigate]);
+
+  // const shouldRedirect = (id) => Object.keys(redirectMap).includes(String(id));
+
+  // useEffect(() => {
+  //   if (shouldRedirect(selectedProductId)) return;
+
+  //   const loadForecastHour = async () => {
+  //     setLoading(true);
+  //     try {
+  //       const { hours } = await fetchForecastHours(
+  //         null,
+  //         null,
+  //         selectedProductId
+  //       );
+  //       if (Array.isArray(hours) && hours.length > 0) {
+  //         dispatch(setSpecialForecastHours(hours));
+  //         dispatch(setSelectedSpecialForecastHour(hours[0]));
+  //       } else {
+  //         dispatch(setSpecialForecastHours([]));
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching forecast hours:', error);
+  //       toast.error('Unexpected error occurred while fetching forecast hours.');
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   loadForecastHour();
+  // }, [selectedProductId]);
+
+  // useEffect(() => {
+  //   if (!hasUserSelectedRef.current || hasNavigatedRef.current) return;
+
+  //   const redirectUrl = redirectMap[selectedProductId];
+  //   if (redirectUrl) {
+  //     hasNavigatedRef.current = true;
+
+  //     if (selectedProductId === 31) {
+  //       navigate(redirectUrl); // internal route
+  //     } else {
+  //       window.location.href = redirectUrl; // external link
+  //     }
+  //   }
+  // }, [selectedProductId, navigate]);
 
   return (
     <div className="h-screen overflow-auto p-6">
